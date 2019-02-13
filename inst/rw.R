9ee6a3a4b4bfacb0bf46be5a23abe4e821438490
Sean Anderson <sean@seananderson.ca>
2019-02-12 18:02:41 -0800

    Play with area under the curves

--- inst/rw.R
+++ inst/rw.R
@@ -38,3 +38,68 @@ ggplot(pred, aes(x = x, ymax = y_hat + year * 5, ymin = year * 5,
   ylab("Predicted density in some units") +
   gfplot::theme_pbs() +
   coord_flip(xlim = c(0, -350))
+
+# --------------------------
+# integrate:
+
+f <- function(x) {
+  exp(r$b_j[1] + r$b_rw_t[1,1] * x + r$b_rw_t[1,2] * x^2)
+}
+
+depth2scaled <- function(x) {
+  x <- -x
+  (log(x) - d$depth_mean[1]) / d$depth_sd[1]
+}
+
+total <- integrate(f,
+  upper = depth2scaled(-400),
+  lower = depth2scaled(-1))[[1]]
+total
+
+thresh <- seq(-1.1, -400, length.out = 500)
+areas <- sapply(thresh, function(.x)
+  integrate(f, upper = depth2scaled(.x), lower = depth2scaled(-1))[[1]])
+
+plot(thresh, areas / total)
+plot(areas / total)
+
+thresh[max(which(areas/total < 0.25))]
+thresh[max(which(areas/total < 0.50))]
+thresh[max(which(areas/total < 0.75))]
+
+# -----------------------------------
+# functionize it
+
+get_quants <- function(i) {
+  f <- function(x) {
+    exp(r$b_j[i] + r$b_rw_t[i,1] * x + r$b_rw_t[i,2] * x^2)
+  }
+
+  depth2scaled <- function(x) {
+    x <- -x
+    (log(x) - d$depth_mean[1]) / d$depth_sd[1]
+  }
+
+  total <- integrate(f,
+    upper = depth2scaled(-500),
+    lower = depth2scaled(-1))[[1]]
+
+  thresh <- seq(-1.1, -500, length.out = 1000)
+  areas <- sapply(thresh, function(.x)
+    integrate(f, upper = depth2scaled(.x), lower = depth2scaled(-1))[[1]])
+
+  data.frame(
+    lwr = thresh[max(which(areas/total < 0.25))],
+    mid = thresh[max(which(areas/total < 0.50))],
+    upr = thresh[max(which(areas/total < 0.75))],
+    year = yrs[i])
+}
+
+out <- purrr::map_df(seq_along(unique(d$year)), get_quants)
+
+ggplot(out, aes(x = year, ymax = upr, ymin = lwr, y = mid)) +
+  geom_ribbon(fill = "grey50", alpha = 0.4) +
+  geom_line() +
+  xlab("Depth (m)") +
+  ylab("Predicted density in some units") +
+  gfplot::theme_pbs()
