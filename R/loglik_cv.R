b11a245073ea86cbf796299fa8fbd7659a0400a1
Sean Anderson <sean@seananderson.ca>
2019-04-12 08:39:58 -0700

    Add comments in the cross-validation function

--- R/loglik_cv.R
+++ R/loglik_cv.R
@@ -2,6 +2,8 @@
 
 ll_gaussian <- function(object, residuals) {
   dispersion <- exp(object$model$par[["ln_phi"]])
+  # FIXME: This shouldn't be the residuals. The testing data
+  # should be x, and the prediction should be mean.
   dnorm(x = residuals, mean = mean(residuals), sd = dispersion, log = TRUE)
 }
 
@@ -14,17 +16,17 @@ ll_sdmTMB <- function(object, residuals, ...) {
   family_func(object, residuals)
 }
 
+# FIXME: Make sure to add Roxygen documentation above for this function.
 loglik_cv <- function(all_data, time = "year", k_folds = 10, fold_id = NULL, n_knots = NULL, ...) {
 
   # split data by 'time' from sdmTMB model arguments
   split_time <- all_data[[time]]
-  k <- k_folds
 
   # add column of fold_ids stratified across time steps
   dd <- lapply(split(all_data, split_time), function(x) {
     obs <- nrow(x)
-    i <- obs / k
-    i <- round(c(0, i * 1:(k - 1), obs))
+    i <- obs / k_folds
+    i <- round(c(0, i * seq(1, (k_folds - 1)), obs))
     times <- i[-1] - i[-length(i)]
     group <- c()
     for (j in 1:(length(times))) {
@@ -34,14 +36,18 @@ loglik_cv <- function(all_data, time = "year", k_folds = 10, fold_id = NULL, n_k
     x$fold_ids <- group[r]
     x
   })
+  # FIXME: Do we need the as.data.frame here?
+  # If we do then we should adjust the code above so that we do not need it.
   d <- as.data.frame(do.call(rbind, dd))
 
   # model data k times for for k-1 folds
-  out <- lapply(1:k_folds, function(k) {
+  out <- lapply(seq_len(k_folds), function(k) {
     d_fit <- d[d$fold_ids != k, , drop = FALSE]
     d_withheld <- d[d$fold_ids == k, , drop = FALSE]
 
     # build mesh for training data
+    # FIXME: we should have the user pass the x and y column names so this can work with
+    # any column names. Also, we should let the user pass a custom inla mesh.
     d_fit_spde <- make_spde(d_fit$X, d_fit$Y, n_knots = n_knots)
 
     # run model
@@ -57,14 +63,16 @@ loglik_cv <- function(all_data, time = "year", k_folds = 10, fold_id = NULL, n_k
     cv_data <- d_withheld
     cv_data$cv_est <- predicted$data$est
 
+    # FIXME: We don't need to calculate the residuals.
     # calculate residuals
     response <- object$formula[[2]]
     residuals <- predicted$data[[response]] - predicted$data$est
 
-    # calculate negative loglikelihood for each withheld observation
+    # calculate log likelihood for each withheld observation
     cv_data$cv_loglik <- ll_sdmTMB(object, residuals)
     cv_data
 
+    # FIXME: The model object is `object` as defined above.
     # would be nice to save model object too, but not sure how to
     # list(cv_data, object)
   })
