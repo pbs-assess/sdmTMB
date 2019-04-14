12d336e43d48f55560bf64f9d9542d08f505d2c7
ecophilina <paenglis@lakeheadu.ca>
2019-04-14 16:43:34 -0700

    add seed

--- R/cross-val.R
+++ R/cross-val.R
@@ -31,6 +31,7 @@ ll_sdmTMB <- function(object, withheld_y, withheld_mu) {
 #' @param spde_function A function that takes 3 arguments (x, y, n_knots) and
 #'   returns a list structure that matches output of [make_spde()].
 #' @param plot_spde Logical for whether or not to produce a plot of each mesh.
+#' @param seed Provide seed to ensure fold pattern is the same for comparison purposes.
 #' @param ... All other arguments required to run sdmTMB model with the
 #'   exception of `data` and `spde` which are redefined for each fold within the
 #'   function.
@@ -60,7 +61,7 @@ ll_sdmTMB <- function(object, withheld_y, withheld_mu) {
 #' }
 sdmTMB_cv <- function(formula, data, time = "year", x = "X", y = "Y",
                       k_folds = 10, fold_ids = NULL, n_knots = NULL,
-                      spde_function = make_spde, plot_spde = FALSE, ...) {
+                      spde_function = make_spde, plot_spde = FALSE, seed = 999, ...) {
   data[["_sdm_order_"]] <- seq_len(nrow(data))
 
   # add column of fold_ids stratified across time steps
@@ -73,6 +74,7 @@ sdmTMB_cv <- function(formula, data, time = "year", x = "X", y = "Y",
       group <- c()
       for (j in seq_along(times))
         group <- c(group, rep(j, times = times[j]))
+      set.seed(seed)
       r <- order(stats::runif(obs))
       x$cv_fold <- group[r]
       x
@@ -81,6 +83,7 @@ sdmTMB_cv <- function(formula, data, time = "year", x = "X", y = "Y",
     fold_ids <- "cv_fold"
   }
 
+#FIXME: with future_lapply, plots do not appear.
   if (plot_spde) {
     op <- graphics::par(
       mfrow = c(ceiling(sqrt(k_folds)), ceiling(sqrt(k_folds))),
@@ -120,5 +123,6 @@ sdmTMB_cv <- function(formula, data, time = "year", x = "X", y = "Y",
   data <- data[order(data[["_sdm_order_"]]), , drop = FALSE]
   data[["_sdm_order_"]] <- NULL
   row.names(data) <- NULL
+#FIXME: Should models that do not converge be treated differently? Should we still calculate ll for them? Can we at least add a column to data or this list that saves the warning?
   list(data = data, models = models, sum_loglik = sum(data$cv_loglik))
 }
