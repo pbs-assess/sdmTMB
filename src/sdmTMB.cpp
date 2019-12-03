3d984fd353a6b3dcf99035df612bc7fe35f76589
Sean Anderson <sean@seananderson.ca>
2019-12-03 14:28:15 -0800

    Fix time-varying prediction bug

--- src/sdmTMB.cpp
+++ src/sdmTMB.cpp
@@ -408,7 +408,7 @@ Type objective_function<Type>::operator()()
     if (random_walk) {
       for (int i = 0; i < proj_X_rw_ik.rows(); i++) {
         for (int k = 0; k < proj_X_rw_ik.cols(); k++) {
-          proj_rw_i(i) = proj_X_rw_ik(i, k) * b_rw_t(proj_year(i), k);
+          proj_rw_i(i) += proj_X_rw_ik(i, k) * b_rw_t(proj_year(i), k);
           proj_fe(i) += proj_rw_i(i);
         }
       }
