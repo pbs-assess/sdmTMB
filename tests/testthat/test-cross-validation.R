2168e0f1bea16a33612a1f98903c5d8e8c4a1dc6
Sean Anderson <sean@seananderson.ca>
2020-09-26 21:36:07 -0700

    Fix test

--- tests/testthat/test-cross-validation.R
+++ tests/testthat/test-cross-validation.R
@@ -1,22 +1,17 @@
 context("Cross validation")
 
 test_that("Basic cross validation works", {
-  d <- subset(pcod, year >= 2015) # subset for example speed
+  d <- subset(pcod, year >= 2011) # subset for example speed
+  spde <- make_spde(d$X, d$Y, n_knots = 30)
+
+  # library(future) # for parallel processing
+  # plan(multisession) # for parallel processing
   x <- sdmTMB_cv(
-    formula = density ~ 1,
-    d, family = tweedie(link = "log"), time = "year", x = "X", y = "Y",
-    n_knots = 20, k_folds = 2, mgcv = FALSE
+    density ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+    data = d, spde = spde,
+    family = tweedie(link = "log"), time = "year", k_folds = 2
   )
   expect_equal(class(x$sum_loglik), "numeric")
   expect_true("data.frame" %in% class(x$data))
   expect_equal(class(x$models[[1]]), "sdmTMB")
-})
-
-test_that("Cross validation works with Gaussian and no time element", {
-  set.seed(123)
-  s <- sim(time_steps = 1, sigma_O = 0.2)
-  x <- sdmTMB_cv(formula = observed ~ 0, data = s, x = "x", y = "y",
-    family = gaussian(link = "identity"), n_knots = 30, k_folds = 2,
-    mgcv = FALSE)
-  expect_equal(class(x$sum_loglik), "numeric")
 })
