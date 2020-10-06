999105b9867b044ee51d885bc6b8bb23ac81fddf
Sean Anderson <sean@seananderson.ca>
2020-10-06 10:07:58 -0700

    Barrier example tweaks

--- inst/barrier-mgcv.R
+++ inst/barrier-mgcv.R
@@ -8,28 +8,32 @@ require(mgcv)
 fsb <- list(fs.boundary())
 nmax <- 100
 ## Simulate some fitting data, inside boundary...
-set.seed(123)
-n<-140
+set.seed(25)
+n<-120
 v <- runif(n)*5-1;w<-runif(n)*2-1
 y <- fs.test(v,w,b=1)
 names(fsb[[1]]) <- c("v","w")
 ind <- inSide(fsb,x=v,y=w) ## remove outsiders
-beta <- 0.2
+beta <- 0
 covariate <- runif(n)
 y <- y + beta * covariate + rnorm(n)*.1 ## add noise
 y <- y[ind];v <- v[ind]; w <- w[ind]; covariate <- covariate[ind]
 n <- length(y)
 
 library(ggplot2)
-ggplot(data.frame(x = v, y = w, z = y), aes(x, y, colour = z)) + geom_point() +
-  scale_colour_viridis_c()
 
 dat <- data.frame(x = v, y = w, z = y, b = covariate)
 library(sdmTMB)
-spde <- make_spde(dat, c("x", "y"), cutoff = 0.01)
+# spde <- make_spde(dat, c("x", "y"), cutoff = 0.01)
+
+xym <- cbind(fsb[[1]]$v, fsb[[1]]$w)
+ggplot(data.frame(x = v, y = w, z = y), aes(x, y, colour = z, size = z)) + geom_point() +
+  scale_colour_gradient2() +
+  geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
+
 
 loc_xy <- cbind(dat$x, dat$y)
-bnd <- INLA::inla.nonconvex.hull(loc_xy, convex = .5)
+bnd <- INLA::inla.nonconvex.hull(loc_xy, convex = .6)
 mesh <- INLA::inla.mesh.2d(
   boundary = bnd,
   max.edge = c(0.3, .5),
@@ -42,7 +46,7 @@ plot(sp2)
 
 spde <- sp2
 plot(spde)
-m <- sdmTMB(z ~ 1 + b, data = dat, spde = spde, silent = FALSE)
+m <- sdmTMB(z ~ 1, data = dat, spde = spde, silent = FALSE)
 grid <- expand.grid(x = seq(min(dat$x) - 0.3, max(dat$x) + 0.3, length.out = 200),
   y = seq(min(dat$y) - 0.3, max(dat$y) + 0.3, length.out = 200), b = mean(covariate))
 
@@ -92,44 +96,46 @@ barrier_spde <- inla.barrier.fem(mesh, barrier.triangles = barrier.triangles)
 bspde <- spde
 bspde$spde_barrier <- barrier_spde
 
-mb <- sdmTMB(z ~ 1 + b, data = dat, spde = bspde, silent = FALSE, barrier = TRUE,
-  barrier_scaling = c(1, .1))
+mb <- sdmTMB(z ~ 1, data = dat, spde = bspde, silent = FALSE, barrier = TRUE,
+  barrier_scaling = c(1, .05))
 
 pb <- predict(mb, newdata = grid)
 
-ggplot(pb, aes(x, y, fill = est)) + geom_raster() +
-  geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
-  scale_fill_gradient2() +
-  geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
-
-ggplot(pred, aes(x, y, fill = est)) + geom_raster() +
-  geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
-  scale_fill_gradient2() +
-  geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
-
+# ggplot(pb, aes(x, y, fill = est)) + geom_raster() +
+#   geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
+#   scale_fill_gradient2() +
+#   geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
+#
+# ggplot(pred, aes(x, y, fill = est)) + geom_raster() +
+#   geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
+#   scale_fill_gradient2() +
+#   geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
+#
 m
 mb
 
-pb$diff <- scale(pred$omega_s, scale = FALSE) - scale(pb$omega_s, scale = FALSE)
-pb$diff <- pred$est - pb$est
-
-theme_set(theme_void())
-ggplot(pb, aes(x, y, fill = diff)) + geom_raster() +
-  scale_fill_gradient2() +
-  geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
-  scale_fill_gradient2() +
-  geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
+# pb$diff <- scale(pred$omega_s, scale = FALSE) - scale(pb$omega_s, scale = FALSE)
+# pb$diff <- pred$est - pb$est
+#
+# theme_set(theme_void())
+# ggplot(pb, aes(x, y, fill = diff)) + geom_raster() +
+#   scale_fill_gradient2() +
+#   geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
+#   scale_fill_gradient2() +
+#   geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
 
 cols <- RColorBrewer::brewer.pal(3, "BrBG")
-g1 <- ggplot(pred, aes(x, y, fill = omega_s)) + geom_raster() +
+g1 <- ggplot(pred, aes(x, y, fill = est)) + geom_raster() +
   geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
-  scale_fill_gradient2(high = cols[1], low = cols[3], mid = cols[2]) + theme(legend.position = "none") +
+  scale_fill_gradient2(high = cols[1], low = cols[3], mid = cols[2]) +
+  # theme(legend.position = "none") +
   geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
 
-g2 <- ggplot(pb, aes(x, y, fill = omega_s)) +
+g2 <- ggplot(pb, aes(x, y, fill = est)) +
   geom_raster() +
   geom_point(data = dat, aes(x, y, size = z), inherit.aes = FALSE, pch = 21) +
-  scale_fill_gradient2(high = cols[1], low = cols[3], mid = cols[2]) + theme(legend.position = "none") +
+  scale_fill_gradient2(high = cols[1], low = cols[3], mid = cols[2]) +
+  # theme(legend.position = "none") +
   geom_path(data = as.data.frame(xym), aes(V1, V2), inherit.aes = FALSE)
 
 cowplot::plot_grid(g1, g2)
