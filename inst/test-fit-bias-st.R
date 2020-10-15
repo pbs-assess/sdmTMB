b512648b322e981dead04ebba7b8148c9c5d14c7
Sean Anderson <sean@seananderson.ca>
2020-10-15 13:10:09 -0700

    Expand bias/coverage testing

--- inst/test-fit-bias-st.R
+++ inst/test-fit-bias-st.R
@@ -3,7 +3,7 @@ library(testthat)
 library(ggplot2)
 library(sdmTMB)
 library(future)
-plan(multisession, workers = floor(availableCores() / 2) - 1)
+plan(multisession, workers = floor(availableCores() / 2))
 options(future.rng.onMisuse = "ignore")
 
 SEED <- 42
@@ -21,19 +21,19 @@ X <- model.matrix(~ x1, data.frame(x1 = rnorm(N * time_steps)))
 
 true <- tribble(
   ~variable, ~true_value,
-  "sigma_E", sigma_E,
+  "sigma_E.est", sigma_E,
   "b0.est", betas[1],
   "b1.est", betas[2],
-  "range", .range,
-  "phi", phi,
-  "rho", rho
+  "range.est", .range,
+  "phi.est", phi,
+  "rho.est", rho
 )
 
 loc <- data.frame(x = x, y = y)
 spde <- make_mesh(loc, xy_cols = c("x", "y"), cutoff = 0.1)
 plot(spde)
 
-out <- furrr::future_map(seq_len(80), function(i) {
+out <- furrr::future_map(seq_len(8*3), function(i) {
   s <- sdmTMB_sim(
     x = x, y = y, mesh = spde, X = X, sigma_V = c(0, 0),
     betas = betas, time_steps = time_steps, ar1_phi = rho,
@@ -52,21 +52,25 @@ out <- furrr::future_map(seq_len(80), function(i) {
   )
   est <- tidy(m, conf.int = TRUE)
   est_ran <- tidy(m, "ran_pars", conf.int = TRUE)
-  p <- as.list(m$model$par)
-  r <- m$tmb_obj$report()
   data.frame(
-    sigma_E = r$sigma_E,
     b0.est = est[est$term == "(Intercept)", "estimate"],
     b0.lwr = est[est$term == "(Intercept)", "conf.low"],
     b0.upr = est[est$term == "(Intercept)", "conf.high"],
     b1.est = est[est$term == "x1", "estimate"],
     b1.lwr = est[est$term == "x1", "conf.low"],
     b1.upr = est[est$term == "x1", "conf.high"],
-    rho.est = r$rho,
-    rho.lwr = est_ran[est_ran$term == "ar1_phi", "conf.low"],
-    rho.upr = est_ran[est_ran$term == "ar1_phi", "conf.high"],
-    range = r$range,
-    phi = exp(p$ln_phi)
+    rho.est = est_ran[est_ran$term == "rho", "estimate"],
+    rho.lwr = est_ran[est_ran$term == "rho", "conf.low"],
+    rho.upr = est_ran[est_ran$term == "rho", "conf.high"],
+    sigma_E.est = est_ran[est_ran$term == "sigma_E", "estimate"],
+    sigma_E.lwr = est_ran[est_ran$term == "sigma_E", "conf.low"],
+    sigma_E.upr = est_ran[est_ran$term == "sigma_E", "conf.high"],
+    phi.est = est_ran[est_ran$term == "phi", "estimate"],
+    phi.lwr = est_ran[est_ran$term == "phi", "conf.low"],
+    phi.upr = est_ran[est_ran$term == "phi", "conf.high"],
+    range.est = est_ran[est_ran$term == "range", "estimate"],
+    range.lwr = est_ran[est_ran$term == "range", "conf.low"],
+    range.upr = est_ran[est_ran$term == "range", "conf.high"]
   )
 })
 
@@ -102,7 +106,10 @@ theme_set(ggsidekick::theme_sleek())
 g1 <- worm_plot(est, rho.lwr, rho.est, rho.upr, rho, expression(rho))
 g2 <- worm_plot(est, b0.lwr, b0.est, b0.upr, betas[1], expression(beta[0]))
 g3 <- worm_plot(est, b1.lwr, b1.est, b1.upr, betas[2], expression(beta[1]))
-cowplot::plot_grid(g1, g2, g3, ncol = 3)
+g3 <- worm_plot(est, phi.lwr, phi.est, phi.upr, phi, expression(phi))
+g4 <- worm_plot(est, sigma_E.lwr, sigma_E.est, sigma_E.upr, sigma_E, expression(sigma[Epsilon]))
+g5 <- worm_plot(est, range.lwr, range.est, range.upr, .range, expression(range))
+cowplot::plot_grid(g1, g2, g3, g4, g5, ncol = 3)
 
 coverage <- est %>%
   mutate(covered = b1.lwr < betas[2], b1.upr > betas[2]) %>%
