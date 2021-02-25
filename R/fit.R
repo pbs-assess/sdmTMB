7727c4f37609d1a1630b384691fcd4205bf435a6
Eric Ward <eric.ward@noaa.gov>
2021-02-25 09:50:19 -0800

    update documentation in fit as it relates to binomial
    include 4 examples of how it can be formulated

--- R/fit.R
+++ R/fit.R
@@ -18,7 +18,13 @@ NULL
 #' @param family The family and link. Supports [gaussian()], [Gamma()],
 #'   [binomial()], [poisson()], \code{\link[sdmTMB:families]{Beta()}},
 #'   \code{\link[sdmTMB:families]{nbinom2()}}, and
-#'   \code{\link[sdmTMB:families]{tweedie()}}].
+#'   \code{\link[sdmTMB:families]{tweedie()}}]. Following the structure of
+#'   [glmmTMB()], a binomial family can be specified in one of 4 ways: (1) the
+#'   response may be a factor (and the model classifies the first level versus
+#'   all others), (2) the response may be binomial (0/1), (3) the response
+#'   can be a matrix of form cbind(success, failure), and (4) the response may
+#'   be the observed proportions, and the 'weights' argument is used to specify
+#'   the Binomial size (N) parameter.
 #' @param time_varying An optional formula describing covariates that should be
 #'   modelled as a random walk through time. Be careul not to include
 #'   covariates (including the intercept) in both the main and time-varying
