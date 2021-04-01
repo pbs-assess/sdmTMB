6ac271a9e7ff6bece00b60b363e4d82f9ce06c61
Sean Anderson <sean@seananderson.ca>
2021-04-01 15:14:51 -0700

    Update Matrix warning message
    https://github.com/pbs-assess/sdmTMB/commit/0be5d12640045a518317883f90b9a64b890fb805#commitcomment-49000517

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,15 @@
 .onAttach <- function(libname, pkgname) {
   if (utils::packageVersion("Matrix") >= package_version('1.3.0')) {
     packageStartupMessage(
-      "There is currently a bug involving versions of the Matrix package >= 1.3-0 ",
-      "that results in sdmTMB models eventually failing to fit with errors about ",
-      "`par.random : replacement has length zero`. ",
-      "If you encounter this, you will want to install an older version of the ",
-      "Matrix package (1.2-18 is suggested). ",
-      "Download the version from here: `https://cran.r-project.org/src/contrib/Archive/Matrix/` ",
-      "and, from the necessary working directory, install with ",
-      "`install.packages('Matrix_1.2-18.tar.gz', type = 'source', repos = NULL)`."
+      "There is currently a bug involving versions of the Matrix package >= 1.3-0 \n",
+      "and the version of TMB on CRAN that results in TMB (and sdmTMB) models \n",
+      "eventually failing to fit with errors about: \n",
+      "`par.random : replacement has length zero`. \n",
+      "If you encounter this, you will want to install the latest versions from source:\n",
+      "\n",
+      "install.packages('Matrix', type = 'source')\n",
+      "remotes::install_github('kaskr/adcomp/TMB')\n",
+      "remotes::install_github('pbs-assess/sdmTMB')\n"
     )
   }
 }
