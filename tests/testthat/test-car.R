cbe857490be226324cacf1ae8a9f88facb03a23e
ericward-noaa <eric.ward@noaa.gov>
2022-01-15 16:20:27 -0800

    limits on alpha give good estimates of: alpha, B0, slope for simulated data

--- tests/testthat/test-car.R
+++ tests/testthat/test-car.R
@@ -5,100 +5,55 @@ test_that("Forecasting works with a 1D Car model", {
   # Good Stan refs, e.g. https://mc-stan.org/users/documentation/case-studies/mbjoseph-CARStan.html
   # demonstrate simple CAR model
   set.seed(123)
-  N = 50
+  N = 10
 
-  W = matrix(0, N, N)
-  for(i in 1:(N-1)) {
-    W[i+1,i] = W[i,i+1]=1
-  }
-  CAR_nb = W + 2*diag(N)
-  CAR_nb[cbind(c(1,N), c(1,N))] = 1
-  D = diag(CAR_nb)
-  alpha = 0.3
-  tau = 0.1
-  sigma_inv = tau * (D - alpha*W)
-  Sigma = as.matrix(Matrix::forceSymmetric(solve(sigma_inv)))
-  re = mvtnorm::rmvnorm(1, mean = rep(0, N), sigma = Sigma)
+  grid <- expand.grid(x = 1:N, y = 1:N)
+  n <- nrow(grid)
+  distance <- as.matrix(dist(grid))
+  W <- array(0, c(n, n))
+  W[distance == 1] <- 1
 
-  # spatial data
-  spat_data = data.frame(car_region = 1:N,
-                         year = 1,
-                         lon = runif(N),
-                         lat = runif(N),
-                         re = c(re))
-  df = data.frame(car_region=sample(1:N,1000,replace=T),
-                  x = runif(1000, 0, 5),
-                  resid = rnorm(1000, 0, 0.03))
-  df = dplyr::left_join(df, spat_data)
-  df$y = 30 + df$x*0.1 * df$x + df$re + df$resid
-
-  # try with limits, no priors
-  m <- sdmTMB(y ~ x, data = df, time = "year",
-              mesh = make_mesh(df,c("lon","lat"),n_knots=8),
-              spatiotemporal = "off",
-              spatial = "on",
-              CAR_neighbours = CAR_nb,
-              control = sdmTMBcontrol(lower = list(car_alpha_s = -1),
-                                      upper = list(car_alpha_s = 1)))
-
-  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_car_tau_s")], log(tau), tolerance = 1e-3)
-  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="car_alpha_s")], alpha, tolerance = 1e-3)
-
-
-  # Try a scenario with a more complicated adjacency matrix W
-  W = matrix(rbinom(N*N,size=1,prob=0.2), N, N)
-  W[upper.tri(W)] <- t(W)[upper.tri(W)]
-  diag(W) = 0
-  diag(W) = apply(W,1,sum)
-
+  #W = matrix(0, N, N)
+  #for(i in 1:(N-1)) {
+  #  W[i+1,i] = W[i,i+1]=1
+  #}
+  #CAR_nb = W + 2*diag(N)
+  #CAR_nb[cbind(c(1,N), c(1,N))] = 1
   CAR_nb = W
-  D = diag(CAR_nb)
-  alpha = 0.3
-  tau = 0.1
-  diag(W) = 0
-  sigma_inv = tau * (D - alpha*W)
-  Sigma = as.matrix(Matrix::forceSymmetric(solve(sigma_inv)))
-  re = mvtnorm::rmvnorm(1, mean = rep(0, N), sigma = Sigma)
+  diag(CAR_nb) = rowSums(W)
+  D = rowSums(W)
+  alpha = 0.8
+  tau = 2
+  ln_phi = 0.03
+  Tau <- tau*(diag(rowSums(W)) - alpha*W)
+
+  re = mvtnorm::rmvnorm(1, mean = rep(0, nrow(W)), sigma = solve(Tau))
 
   # spatial data
-  spat_data = data.frame(car_region = 1:N,
+  spat_data = data.frame(car_region = 1:nrow(W),
                          year = 1,
-                         lon = runif(N),
-                         lat = runif(N),
-                         re = c(re))
-  df = data.frame(car_region=sample(1:N,1000,replace=T),
-                  x = runif(1000, 0, 5),
-                  resid = rnorm(1000, 0, 0.03))
-  df = dplyr::left_join(df, spat_data)
-  df$y = 3 + 0.1 * df$x + df$re + df$resid
+                         lon = runif(nrow(W)),
+                         lat = runif(nrow(W)),
+                         x = rnorm(nrow(W),3,1))
+  spat_data$y = 30 + spat_data$x*0.1 + c(re) + rnorm(nrow(W), 0, exp(ln_phi))
 
   # try with limits, no priors
-  m <- sdmTMB(y ~ x, data = df, time = "year",
-              mesh = make_mesh(df,c("lon","lat"),n_knots=8),
+  m <- sdmTMB(y ~ x, data = spat_data, time = "year",
+              mesh = make_mesh(spat_data,c("lon","lat"),n_knots=8),
               spatiotemporal = "off",
               spatial = "on",
               CAR_neighbours = CAR_nb,
-              control = sdmTMBcontrol(lower = list(car_alpha_s = -1),
-                                      upper = list(car_alpha_s = 1)))
+              control = sdmTMBcontrol(lower = list(car_alpha_s = 0.1),
+                                      upper = list(car_alpha_s = 0.9)))
 
-  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_car_tau_s")], log(tau), tolerance = 1e-3)
-  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="car_alpha_s")], alpha, tolerance = 1e-3)
+  ln_car_tau_s = log(tau)
+  names(ln_car_tau_s) = "ln_car_tau_s"
+  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_car_tau_s")], ln_car_tau_s, tolerance = 1e-3)
 
+  names(alpha) = "car_alpha_s"
+  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="car_alpha_s")], alpha, tolerance = 1e-2)
 
-  set.seed(33)
-  n.torus <- 10
-  rho <- 0.2
-  sigma <- 1.5
-  prec <- 1/sigma
-  beta <- c(1, 1)
-  XX <- cbind(rep(1, n.torus^2), sample(log(1:n.torus^2)/5))
-  mydata1 <- CAR.simTorus(n1 = n.torus, n2 = n.torus, rho = rho, prec = prec)
-  ## Simulate CAR data for a given spatial weight matrix
-  Wmat <- mydata1$W
-  mydata2 <- CAR.simWmat(rho = rho, prec = prec, W = Wmat)
-  ## Simualte data from a linear model with CAR error
-  y <- XX %*% beta + mydata1$X
-  mydata1$data.vec <- data.frame(y=y, XX[,-1])
-  mydata3 <- CAR.simLM(pars = c(0.1, 1, 2, 0.5), data = mydata1)
+  names(ln_phi) = "ln_phi"
+  expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_phi")],ln_phi, tolerance = 1e-2)
 
 })
