d87da75c2c66f19f3f24e73dc093060ad3c529fa
Sean Anderson <sean@seananderson.ca>
2022-01-17 10:03:22 -0800

    Tweak CAR plot

--- tests/testthat/test-car.R
+++ tests/testthat/test-car.R
@@ -5,7 +5,7 @@ test_that("Forecasting works with a 1D Car model", {
   # Good Stan refs, e.g. https://mc-stan.org/users/documentation/case-studies/mbjoseph-CARStan.html
   # demonstrate simple CAR model
   set.seed(123)
-  N = 20
+  N = 10
 
   grid <- expand.grid(x = 1:N, y = 1:N)
   n <- nrow(grid)
@@ -18,7 +18,7 @@ test_that("Forecasting works with a 1D Car model", {
   D = rowSums(W)
   alpha = 0.8
   tau = 2
-  B0 = 30
+  B0 = 0.5
 
   Tau <- tau*(diag(rowSums(W)) - alpha*W)
 
@@ -29,13 +29,21 @@ test_that("Forecasting works with a 1D Car model", {
                          year = 1,
                          lon = runif(nrow(W)), # dummy
                          lat = runif(nrow(W))) # dummy
-  ln_phi = log(0.01)
+
+  # library(ggplot2)
+  # ggplot(spat_data, aes(lon, lat)) + geom_point()
+
+  ln_phi = log(0.1)
   df = data.frame(car_region = 1:nrow(W),
                   x = rnorm(nrow(W),3,1),
                   resid = rnorm(nrow(W),0,exp(ln_phi)))
   df = dplyr::left_join(df,spat_data)
 
+  df$mu = B0 + re[df$car_region]
   df$y = B0 + re[df$car_region] + df$resid
+
+  # ggplot(df, aes(lon, lat, colour = mu)) + geom_point() + scale_color_viridis_c()
+  # ggplot(df, aes(lon, lat, colour = y)) + geom_point() + scale_color_viridis_c()
 
   # try with limits, no priors
   m <- sdmTMB(y ~ 1, data = df, time = "year",
@@ -58,3 +66,5 @@ test_that("Forecasting works with a 1D Car model", {
   expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_phi")],ln_phi, tolerance = 1e-2)
 
 })
+
+
