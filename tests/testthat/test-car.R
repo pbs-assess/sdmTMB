e7ba6e1747828322eff352f270fe3b7a0617873d
ericward-noaa <eric.ward@noaa.gov>
2022-01-26 17:07:06 -0800

    tests failing

--- tests/testthat/test-car.R
+++ tests/testthat/test-car.R
@@ -1,4 +1,4 @@
-test_that("Test 1D CAR model works", {
+test_that("Test CAR model works with 1 time slice", {
   skip_on_cran()
   skip_on_ci()
   skip_if_not_installed("INLA")
@@ -57,7 +57,7 @@ test_that("Test 1D CAR model works", {
 
   # test with several years of data
   df = expand.grid(car_region = 1:nrow(W),
-                  year = 1:2)
+                  year = 1:5)
   #df = dplyr::left_join(df,spat_data)
   df$resid = rnorm(nrow(W),0,exp(-1.203973))
   df$mu = B0 + re[df$car_region]
@@ -67,21 +67,120 @@ test_that("Test 1D CAR model works", {
   m <- sdmTMB(y ~1, data = df, time = "year",
               spatiotemporal = "off",
               spatial = "on",
-              CAR_neighbours = CAR_nb)
-
-  # names(B0) = "b_j"
-  # expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="b_j")], B0, tolerance = 1e-2)
-  #
-  # est_ln_tau_inv = as.numeric(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_car_tau_s")])
-  # est_tau = (1/exp(est_ln_tau_inv))^2
-  # expect_equal(est_tau, tau, tolerance = 0.1)
-  #
-  # alpha_est = as.numeric(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="logit_car_alpha_s")])
-  # expect_equal(plogis(alpha_est), alpha, tolerance = 0.07)
-  #
-  # names(ln_phi) = "ln_phi"
-  # expect_equal(m$sd_report$par.fixed[which(names(m$sd_report$par.fixed)=="ln_phi")],ln_phi, tolerance = 1e-2)
+              CAR_neighbours = CAR_nb,
+              control = sdmTMBcontrol(lower = list(logit_car_alpha_s = -10, ln_car_tau_s = -5),
+                                      upper = list(logit_car_alpha_s = 10, ln_car_tau_s = 4)))
 
 })
 
+test_that("Test CAR model works with several time slices", {
+  skip_on_cran()
+  skip_on_ci()
+  skip_if_not_installed("INLA")
+
+  set.seed(123)
+  N = 10
+
+  grid <- expand.grid(x = 1:N, y = 1:N)
+  n <- nrow(grid)
+  distance <- as.matrix(dist(grid))
+  W <- array(0, c(n, n))
+  W[distance == 1] <- 1
+
+  CAR_nb = W
+  diag(CAR_nb) = rowSums(W)
+  D = rowSums(W)
+  alpha = 0.8
+  sigma = 0.5
+  tau <- 1/(sigma*sigma)
+  B0 = 0.5
+
+  Tau <- tau*(diag(rowSums(W)) - alpha*W)
+
+  re = mvtnorm::rmvnorm(1, mean = rep(0, nrow(W)), sigma = solve(Tau))
+
+  # spatial data
+  spat_data = data.frame(car_region = 1:nrow(W),
+                         year = 1,
+                         lon = runif(nrow(W)), # dummy
+                         lat = runif(nrow(W))) # dummy
+
+  # library(ggplot2)
+  # ggplot(spat_data, aes(lon, lat)) + geom_point()
+
+  ln_phi = log(0.1)
+
+  # test with several years of data
+  df = expand.grid(car_region = 1:nrow(W),
+                   year = 1:5)
+  #df = dplyr::left_join(df,spat_data)
+  df$resid = rnorm(nrow(W),0,ln_phi)
+  df$mu = B0 + re[df$car_region]
+  df$y = B0 + re[df$car_region] + df$resid
+
+  #spde = make_mesh(df, c("car_region","car_region"),n_knots=4)
+  m <- sdmTMB(y ~1, data = df, time = "year",
+              spatiotemporal = "off",
+              spatial = "on",
+              CAR_neighbours = CAR_nb,
+              control = sdmTMBcontrol(lower = list(logit_car_alpha_s = -10, ln_car_tau_s = -5),
+                                      upper = list(logit_car_alpha_s = 10, ln_car_tau_s = 4)))
+
+})
+
+
+test_that("Test CAR model works with several time slices", {
+  skip_on_cran()
+  skip_on_ci()
+  skip_if_not_installed("INLA")
+
+  set.seed(123)
+  N = 10
+
+  grid <- expand.grid(x = 1:N, y = 1:N)
+  n <- nrow(grid)
+  distance <- as.matrix(dist(grid))
+  W <- array(0, c(n, n))
+  W[distance == 1] <- 1
+
+  CAR_nb = W
+  diag(CAR_nb) = rowSums(W)
+  D = rowSums(W)
+  alpha = 0.8
+  sigma = 0.5
+  tau <- 1/(sigma*sigma)
+  B0 = 0.5
+
+  Tau <- tau*(diag(rowSums(W)) - alpha*W)
+
+  re = mvtnorm::rmvnorm(1, mean = rep(0, nrow(W)), sigma = solve(Tau))
+
+  # spatial data
+  spat_data = data.frame(car_region = 1:nrow(W),
+                         year = 1,
+                         lon = runif(nrow(W)), # dummy
+                         lat = runif(nrow(W))) # dummy
+
+  # library(ggplot2)
+  # ggplot(spat_data, aes(lon, lat)) + geom_point()
+
+  ln_phi = log(0.1)
+
+  # test with several years of data
+  df = expand.grid(car_region = 1:nrow(W),
+                   year = 1:5)
+  #df = dplyr::left_join(df,spat_data)
+  df$resid = rnorm(nrow(W),0,ln_phi)
+  df$mu = B0 + re[df$car_region]
+  df$y = B0 + re[df$car_region] + df$resid
+
+  #spde = make_mesh(df, c("car_region","car_region"),n_knots=4)
+  m <- sdmTMB(y ~1, data = df, time = "year",
+              spatiotemporal = "iid",
+              spatial = "off",
+              CAR_neighbours = CAR_nb,
+              control = sdmTMBcontrol(lower = list(logit_car_alpha_s = -10, ln_car_tau_s = -5),
+                                      upper = list(logit_car_alpha_s = 10, ln_car_tau_s = 4)))
+
+})
 
