9ae822d9c64c5bc3ec32e7c0a610684e035acb50
Sean Anderson <sean@seananderson.ca>
2022-04-20 23:26:15 -0700

    Add delta tests

--- tests/testthat/test-delta2.R
+++ tests/testthat/test-delta2.R
@@ -181,3 +181,73 @@ test_that("spatiotemporal field mapping/specification works with delta models", 
   s2 <- as.list(fit$sd_report, "Estimate")
   expect_equal(s1, s2)
 })
+
+test_that("delta models work with different main effects", {
+  skip_on_cran()
+  skip_if_not_installed("INLA")
+
+  mesh <- make_mesh(pcod_2011, c("X", "Y"), cutoff = 20)
+
+  fit <- sdmTMB(
+    formula = list(
+      density ~ depth_scaled,
+      density ~ depth_scaled + depth_scaled2
+    ),
+    data = pcod_2011,
+    mesh = mesh,
+    spatial = "off",
+    family = delta_gamma()
+  )
+  s <- as.list(fit$sd_report, "Estimate")
+  se <- as.list(fit$sd_report, "Std. Error")
+  expect_true("b_j2" %in% names(s))
+  expect_true(all(!is.na(se$b_j2)))
+  expect_true(all(!is.na(se$b_j)))
+  tidy(fit)
+  fit
+  p <- predict(fit)
+
+  # still works?
+  fit <- sdmTMB(
+    formula = density ~ depth_scaled + depth_scaled2,
+    data = pcod_2011,
+    mesh = mesh,
+    spatial = "off",
+    family = delta_gamma()
+  )
+  fit
+  p <- predict(fit)
+
+  # one smoother works
+  fit <- sdmTMB(
+    formula = density ~ s(depth_scaled),
+    data = pcod_2011,
+    mesh = mesh,
+    spatial = "off",
+    family = delta_gamma()
+  )
+
+  # should be same:
+  fit2 <- sdmTMB(
+    formula = list(density ~ s(depth_scaled), density ~ s(depth_scaled)),
+    data = pcod_2011,
+    mesh = mesh,
+    spatial = "off",
+    family = delta_gamma()
+  )
+  expect_equal(fit$sd_report, fit2$sd_report)
+
+  # diff smoothers throw an error for now
+  expect_error(
+    {
+      fit <- sdmTMB(
+        formula = list(density ~ s(depth_scaled), density ~ s(year, k = 3)),
+        data = pcod_2011,
+        mesh = mesh,
+        spatial = "off",
+        family = delta_gamma()
+      )
+    },
+    regexp = "smooth"
+  )
+})
