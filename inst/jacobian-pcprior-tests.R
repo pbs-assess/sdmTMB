0c9c894103c7dfa868a76b6473fa075f360a0088
ericward-noaa <eric.ward@noaa.gov>
2022-05-28 06:37:03 -0700

    forgot random

--- inst/jacobian-pcprior-tests.R
+++ inst/jacobian-pcprior-tests.R
@@ -48,7 +48,7 @@ dyn.load(dynlib("inst/pc_matern"))
 data <- dat
 parameters <- list(B0 = 0, ln_tau_O = 0, ln_kappa = 0, ln_sigma=0,
                    omega_s = matrix(0,n_s, 1))
-obj1 <- MakeADFun(data, parameters, DLL = "pc_matern", hessian = TRUE)
+obj1 <- MakeADFun(data, parameters, random = "omega_s", DLL = "pc_matern", hessian = TRUE)
 fit1 <- nlminb(obj1$par, objective = obj1$fn, gradient = obj1$gr,
               control=list(eval.max=10000, iter.max=10000))
 fit1
@@ -62,12 +62,20 @@ data$matern_range <- 0.3 # greater than this is range_prob
 data$range_prob <- 0.05
 data$matern_SD <- 0.01 # less than this is SD_prob
 data$SD_prob <- 0.05
-obj2 <- MakeADFun(data, parameters, DLL = "pc_matern_prior", hessian = TRUE)
+obj2 <- MakeADFun(data, parameters, DLL = "pc_matern_prior", random = "omega_s", hessian = TRUE)
 fit2 <- nlminb(obj2$par, objective = obj2$fn, gradient = obj2$gr,
               control=list(eval.max=4000, iter.max=4000))
 fit2
 
 sdr1 = sdreport(obj1)
 sdr2 = sdreport(obj2)
+
+# look at predictions
+sim_dat$pred_1 = sdr1$value[which(names(sdr1$value)=="pred")]
+sim_dat$pred_2 = sdr2$value[which(names(sdr2$value)=="pred")]
+
+# predictions are perfectly correlated with / without prior
+ggplot(sim_dat, aes(pred_1, pred_2)) +
+  geom_point(alpha=0.1, col="blue")
 
 # need to add 3rd case with Jacobian adj
