36d6ce1ea6ebf10d69e64f91c454362995141ab9
Sean Anderson <sean@seananderson.ca>
2022-06-21 07:50:07 -0700

    Style unit test/bump CI

--- tests/testthat/test-1-fit-basic.R
+++ tests/testthat/test-1-fit-basic.R
@@ -19,30 +19,41 @@ test_that("sdmTMB model fit with a covariate beta", {
   set.seed(1)
   loc$cov1 <- runif(nrow(loc))
   s <- sdmTMB_simulate(
-    ~ 0 + cov1, loc, mesh = spde, time = "time", B = initial_betas,
+    ~ 0 + cov1, loc,
+    mesh = spde, time = "time", B = initial_betas,
     phi = phi, range = range, sigma_O = sigma_O, sigma_E = sigma_E,
     seed = SEED
   )
-  expect_equal(round(s$observed[c(1:30, 300)], 3),
-    c(-0.313, -0.408, 0.551, 0.131, 0.675, 0.904, 0.048, 0.554, 0.361,
+  expect_equal(
+    round(s$observed[c(1:30, 300)], 3),
+    c(
+      -0.313, -0.408, 0.551, 0.131, 0.675, 0.904, 0.048, 0.554, 0.361,
       0.439, 0.442, 0.532, -0.134, 0.228, 0.197, 0.403, -0.039, -0.092,
       0.269, 0.277, 0.765, -0.724, -0.108, -0.165, 0.942, -0.085, -0.036,
-      0.631, 0.783, 0.06, 0.199)
+      0.631, 0.783, 0.06, 0.199
+    )
   )
   plot(spde)
   .t1 <- system.time({
-    m <- sdmTMB(data = s, formula = observed ~ 0 + cov1, time = "time",
-      silent = TRUE, mesh = spde, control = sdmTMBcontrol(normalize = FALSE, newton_loops = 1))
+    m <- sdmTMB(
+      data = s, formula = observed ~ 0 + cov1, time = "time",
+      silent = TRUE, mesh = spde, control = sdmTMBcontrol(normalize = FALSE, newton_loops = 1)
+    )
   })
   .t2 <- system.time({
-    m_norm <- sdmTMB(data = s, formula = observed ~ 0 + cov1, time = "time",
-      silent = TRUE, mesh = spde, control = sdmTMBcontrol(normalize = TRUE, newton_loops = 1))
+    m_norm <- sdmTMB(
+      data = s, formula = observed ~ 0 + cov1, time = "time",
+      silent = TRUE, mesh = spde, control = sdmTMBcontrol(normalize = TRUE, newton_loops = 1)
+    )
   })
   .t3 <- system.time({
-    m_pc <- sdmTMB(data = s, formula = observed ~ 0 + cov1, time = "time",
+    m_pc <- sdmTMB(
+      data = s, formula = observed ~ 0 + cov1, time = "time",
       silent = TRUE, mesh = spde, control = sdmTMBcontrol(normalize = FALSE, newton_loops = 1),
       priors = sdmTMBpriors(
-        matern_s = pc_matern(range_gt = 0.2, sigma_lt = 0.2, range_prob = 0.05, sigma_prob = 0.05)))
+        matern_s = pc_matern(range_gt = 0.2, sigma_lt = 0.2, range_prob = 0.05, sigma_prob = 0.05)
+      )
+    )
   })
 
   expect_equal(m$model$par, m_norm$model$par, tolerance = 1e-4)
@@ -56,8 +67,8 @@ test_that("sdmTMB model fit with a covariate beta", {
 
   # PC should make range bigger and sigmaO smaller
   # therefore, ln_kappa smaller
-  expect_true(m_pc$model$par[['ln_kappa']] < m_norm$model$par[['ln_kappa']])
-  expect_true(m_pc$model$par[['ln_kappa']] < m_norm$model$par[['ln_kappa']])
+  expect_true(m_pc$model$par[["ln_kappa"]] < m_norm$model$par[["ln_kappa"]])
+  expect_true(m_pc$model$par[["ln_kappa"]] < m_norm$model$par[["ln_kappa"]])
   r_pc <- m_pc$tmb_obj$report()
   r <- m_norm$tmb_obj$report()
   expect_true(r_pc$range[1] > r$range[1])
@@ -102,10 +113,14 @@ test_that("sdmTMB model fit with a covariate beta", {
 
   # no fields; fake mesh:
   set.seed(1)
-  m <- sdmTMB(data = s, formula = observed ~ 0 + cov1, time = "time",
-    spatial = "off", spatiotemporal = "off")
-  m <- sdmTMB(data = s, formula = observed ~ 0 + cov1, time = "time",
-    spatial = FALSE, spatiotemporal = "off")
+  m <- sdmTMB(
+    data = s, formula = observed ~ 0 + cov1, time = "time",
+    spatial = "off", spatiotemporal = "off"
+  )
+  m <- sdmTMB(
+    data = s, formula = observed ~ 0 + cov1, time = "time",
+    spatial = FALSE, spatiotemporal = "off"
+  )
   m <- sdmTMB(data = s, formula = observed ~ 0 + cov1, spatial = "off")
   m <- sdmTMB(data = s, formula = observed ~ 0 + cov1, spatial = FALSE)
 })
@@ -115,17 +130,21 @@ test_that("Anisotropy fits and plots", {
   skip_on_cran()
   skip_if_not_installed("INLA")
   local_edition(2)
-  m <- sdmTMB(data = pcod,
+  m <- sdmTMB(
+    data = pcod,
     formula = density ~ 0 + as.factor(year),
     mesh = make_mesh(pcod, c("X", "Y"), n_knots = 50, type = "kmeans"),
-    family = tweedie(link = "log"), anisotropy = TRUE)
+    family = tweedie(link = "log"), anisotropy = TRUE
+  )
   expect_identical(class(m), "sdmTMB")
   plot_anisotropy(m)
   expect_equal(m$tmb_obj$report()$H,
     structure(
       c(0.665528444798002, 0.079350716881963, 0.079350716881963, 1.51202633656794),
-      .Dim = c(2L, 2L)),
-    tolerance = 1e-3)
+      .Dim = c(2L, 2L)
+    ),
+    tolerance = 1e-3
+  )
 })
 
 test_that("A spatiotemporal version works with predictions on new data points", {
@@ -169,7 +188,8 @@ test_that("Predictions on the original data set as `newdata`` return the same pr
   loc$time <- rep(1:5, each = 100)
   spde <- make_mesh(loc, c("x", "y"), cutoff = 0.04)
   dat <- sdmTMB_simulate(
-    ~ 1, loc, mesh = spde,
+    ~1, loc,
+    mesh = spde,
     time = "time", range = 0.2, B = 0,
     sigma_O = 0, sigma_E = 0.3, phi = 0.1,
     seed = 1
@@ -187,7 +207,7 @@ test_that("Predictions on the original data set as `newdata`` return the same pr
   tidy(m, effects = "ran_par", conf.int = TRUE)
 
   cols <- c("est", "est_non_rf", "est_rf", "epsilon_st")
-  expect_equal(p[,cols], p_nd[,cols], tolerance = 1e-3)
+  expect_equal(p[, cols], p_nd[, cols], tolerance = 1e-3)
 
   m <- sdmTMB(
     spatiotemporal = "AR1", spatial = FALSE,
@@ -197,7 +217,7 @@ test_that("Predictions on the original data set as `newdata`` return the same pr
 
   p <- predict(m)
   p_nd <- predict(m, newdata = dat)
-  expect_equal(p[,cols], p_nd[,cols], tolerance = 1e-3)
+  expect_equal(p[, cols], p_nd[, cols], tolerance = 1e-3)
 })
 
 test_that("poly() works on newdata", {
@@ -213,7 +233,7 @@ test_that("poly() works on newdata", {
     mesh = mesh, family = sdmTMB::tweedie(link = "log"),
     spatial = "off"
   )
-  nd <- pcod_2011[1:3,]
+  nd <- pcod_2011[1:3, ]
   p <- predict(m, newdata = nd)
   p <- p$est
   m2 <- glmmTMB::glmmTMB(
