de3eae3cf2d2eb085d41e03409771fee42354899
Sean Anderson <sean@seananderson.ca>
2022-10-28 10:40:53 -0700

    Allow prediction on newdata with missing years #130

---
+++ tests/testthat/test-prediction-missing-years.R
@@ -0,0 +1,22 @@
+test_that("Prediction works with missing time", {
+  skip_on_cran()
+  skip_if_not_installed("INLA")
+  fit <- sdmTMB(
+    density ~ 1,
+    data = pcod_2011, mesh = pcod_mesh_2011, time = "year",
+    family = tweedie(link = "log")
+  )
+  nd <- pcod_2011[pcod_2011$year %in% c(2013, 2017), ]
+  p1 <- predict(fit, newdata = nd)
+
+  p2 <- predict(fit, newdata = pcod_2011)
+  p2 <- p2[p2$year %in% c(2013, 2017), ]
+
+  expect_equal(nrow(p1), nrow(p2))
+  expect_equal(p1$est, p2$est)
+  expect_equal(p1$year, p2$year)
+  expect_equal(p1, p2)
+
+  expect_warning(p3 <- predict(fit, newdata = nd, return_tmb_object = TRUE), regexp = "time")
+  expect_error(get_index(p3), regexp = "time")
+})
