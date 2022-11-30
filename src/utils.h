3ec1b87da56cd1edd9189dc3d25aaafde2cdab7c
Sean Anderson <sean@seananderson.ca>
2022-11-29 16:14:25 -0800

    Fix () mistake in utils.h MI

--- src/utils.h
+++ src/utils.h
@@ -215,8 +215,8 @@ Type logistic_threshold(Type x, Type s50, Type s95, Type scale, int mi) {
   // Type s95 = s50 + exp(soffset); // this done outside function
   Type pred;
   if (mi) {
-    pred = scale * Type(1.0) /
-      (Type(1.0) + exp(-log(Type(19.0)) * (x - s50) / s95) - Type(1.0));
+    pred = scale * (Type(1.0) /
+      (Type(1.0) + exp(-log(Type(19.0)) * (x - s50) / s95)) - Type(1.0));
   } else {
     pred = scale * Type(1.0) /
       (Type(1.0) + exp(-log(Type(19.0)) * (x - s50) / (s95 - s50)));
