cfc8817f4773ac47007b292b1a0fc483d46a606e
Sean Anderson <sean@seananderson.ca>
2023-01-11 12:30:15 -0800

    Update data-raw for Latin-1 NOTEs #158

--- data-raw/bc_coast.R
+++ data-raw/bc_coast.R
@@ -5,4 +5,10 @@ map_data <- rnaturalearth::ne_countries(
 bc_coast <- suppressWarnings(suppressMessages(
   st_crop(map_data,
     c(xmin = -134, ymin = 46, xmax = -120, ymax = 57))))
-usethis::use_data(bc_coast)
+
+# avoid Latin-1 NOTEs
+# https://github.com/pbs-assess/sdmTMB/issues/158
+bc_coast$name_es <- "Canada"
+bc_coast$name_pt <- "Canada"
+
+usethis::use_data(bc_coast, overwrite = TRUE)
