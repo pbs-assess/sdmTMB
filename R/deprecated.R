f3cbb3d7313c39422da5ff5d45d5c63e1fa1a74d
Sean Anderson <sean@seananderson.ca>
2023-01-25 21:52:23 -0800

    Move add_barrier_mesh back

--- R/deprecated.R
+++ R/deprecated.R
@@ -12,25 +12,6 @@ extract_mcmc <- function(object = deprecated()) {
 
 xtra_msg <- "Load the sdmTMBextra package to use this function: https://github.com/pbs-assess/sdmTMBextra"
 
-#' Transform a mesh object into a mesh with correlation barriers
-#'
-#' @param spde_obj **Deprecated** See the \pkg{sdmTMBextra} package.
-#' @param barrier_sf **Deprecated** See the \pkg{sdmTMBextra} package.
-#' @param range_fraction **Deprecated** See the \pkg{sdmTMBextra} package.
-#' @param proj_scaling **Deprecated** See the \pkg{sdmTMBextra} package.
-#' @param plot **Deprecated** See the \pkg{sdmTMBextra} package.
-#'
-#' @return
-#' Deprecated See \pkg{sdmTMBextra} package.
-#'
-#' @export
-
-add_barrier_mesh <- function(spde_obj = deprecated(), barrier_sf,
-  range_fraction = 0.2,
-  proj_scaling = 1, plot = FALSE) {
-  deprecate_stop("0.2.2", "add_barrier_mesh()", details = xtra_msg)
-}
-
 #' DHARMa residuals
 #'
 #' @param simulated_response **Deprecated** See the \pkg{sdmTMBextra} package.
