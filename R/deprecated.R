2b26310b855c6a2d480571f6902bbdea1157673b
Sean Anderson <sean@seananderson.ca>
2023-01-26 16:35:39 -0800

    Deprecated function tweaks

--- R/deprecated.R
+++ R/deprecated.R
@@ -6,15 +6,18 @@
 #' @param object **Deprecated** See the \href{https://github.com/pbs-assess/sdmTMBextra}{sdmTMBextra} package.
 #'
 #' @return
-#' Deprecated See the \pkg{sdmTMBextra} package.
-#' <https://github.com/pbs-assess/sdmTMBextra>
+#' Deprecated. See the
+#' \href{https://github.com/pbs-assess/sdmTMBextra}{sdmTMBextra} package.
 #'
+#' @keywords internal
 #' @export
+#' @examples
+#' \dontrun{
+#' extract_mcmc()
+#' }
 extract_mcmc <- function(object = deprecated()) {
-  deprecate_stop("0.2.2", "extract_mcmc()", details = xtra_msg)
+  deprecate_stop("0.2.2", "extract_mcmc()", details = "Load the sdmTMBextra package to use this function: https://github.com/pbs-assess/sdmTMBextra")
 }
-
-xtra_msg <- "Load the sdmTMBextra package to use this function: https://github.com/pbs-assess/sdmTMBextra"
 
 #' DHARMa residuals
 #'
@@ -29,9 +32,14 @@ xtra_msg <- "Load the sdmTMBextra package to use this function: https://github.c
 #' @return
 #' Deprecated. See the
 #' \href{https://github.com/pbs-assess/sdmTMBextra}{sdmTMBextra} package.
+#' @keywords internal
 #' @export
+#' @examples
+#' \dontrun{
+#' dharma_residuals()
+#' }
 
 dharma_residuals <- function(simulated_response = deprecated(), object, plot = TRUE, ...) {
-  deprecate_stop("0.2.2", "dharma_residuals()", details = xtra_msg)
+  deprecate_stop("0.2.2", "dharma_residuals()", details = "Load the sdmTMBextra package to use this function: https://github.com/pbs-assess/sdmTMBextra")
 }
 
