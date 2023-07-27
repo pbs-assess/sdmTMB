948073414b41067bc03ab695592c1da44be66280
Sean Anderson <sean@seananderson.ca>
2023-07-26 18:10:07 -0700

    Add censored likelihood tests

--- scratch/censored.R
+++ scratch/censored.R
@@ -2,6 +2,16 @@ library(dplyr)
 library(ggplot2)
 theme_set(theme_light())
 
+logspace_sub <- function(log_a, log_b) {
+  if (log_a == log_b) {
+    return(-Inf)  # Result should be zero in linear space, which is -Inf in log space
+  } else if (log_a < log_b) {
+    return(log_b + log1p(-exp(log_a - log_b)))
+  } else {
+    return(log_a + log1p(-exp(log_b - log_a)))
+  }
+}
+
 dcens_pois <- function(x, lambda) {
   1 - ppois(x - 1, lambda)
 }
@@ -9,6 +19,68 @@ dcens_pois <- function(x, lambda) {
 dcens_pois_upper <- function(x, lambda, upper) {
   ppois(upper, lambda) - ppois(x - 1, lambda)
 }
+
+dcens_pois_joe <- function(x, lambda) {
+  tmp_ll = log(ppois(x-1, lambda)) # // F(lower-1)
+  tmp_ll = logspace_sub(0, tmp_ll) # // 1 - F(lower-1)
+  exp(tmp_ll)
+}
+
+dcens_pois_upper_joe <- function(x, lambda, upper) {
+  tmp_ll = log(ppois(upper, lambda)) # // F(upr)
+  if (x > 0) {
+    tmp_ll = logspace_sub(tmp_ll, log(ppois(x-1, lambda))) #// F(upr) - F(lwr-1) iff lwr>0
+  }
+  exp(tmp_ll)
+}
+
+dcens_pois_joe(9, 10)
+dcens_pois(9, 10)
+
+dcens_pois_upper(x = 9, lambda = 10, upper = 12)
+dcens_pois_upper_joe(x = 9, lambda = 10, upper = 12)
+dcens_pois_upper(x = 0, lambda = 10, upper = 12)
+dcens_pois_upper_joe(x = 0, lambda = 10, upper = 12)
+
+# -----------------------------
+
+dcenspois_sean <- function(x, lambda, upr, give_log = 0L) {
+  if (is.na(upr)) { # full right censored
+    if (x == 0) {
+      ll <- 0
+    } else {
+      ll <- log(dcens_pois_joe(x, lambda))
+    }
+  } else if (upr > x) { # upper truncated right censored
+    ll <- log(dcens_pois_upper_joe(x, lambda, upr))
+  } else if (x == upr) { # not censored
+    ll <- dpois(x, lambda, log = TRUE)
+  }
+  if (give_log) {
+    return(ll)
+  } else {
+    return(exp(ll))
+  }
+}
+
+dcens_pois_upper(9, 10, 12)
+dcenspois_sean(9, 10, upr = 12)
+
+dcens_pois_upper(9, 10, 12)
+dcenspois_sean(9, 10, upr = 12)
+
+dcens_pois(9, 10)
+dcenspois_sean(9, 10, upr = NA)
+
+dcens_pois(0, 10)
+dcenspois_sean(0, 10, upr = NA)
+
+dcens_pois(0, 10)
+dcenspois_sean(0, 10, upr = 11)
+
+# ------------------
+
+
 
 # x <- seq(1, 40)
 # mu <- 30
