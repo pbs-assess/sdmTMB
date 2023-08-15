cf0854af91e39d51269bb9c77e31592e7ab5d476
Sean Anderson <sean@seananderson.ca>
2023-08-15 11:08:48 -0700

    Fix test [skip ci]

--- tests/testthat/test-cross-validation.R
+++ tests/testthat/test-cross-validation.R
@@ -242,5 +242,5 @@ test_that("Delta model cross validation works", {
     family = delta_poisson_link_gamma(), k_folds = 2
   )
   diff_ll <- out_dpg$sum_loglik - out_dg$sum_loglik
-  expect_equal(round(diff_ll, 4), round(-4.250411, 4))
+  expect_equal(round(diff_ll, 4), round(-4.629497, 4))
 })
