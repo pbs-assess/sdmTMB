b8b5ebbd74fc4cc866a299a71ebf34591bbb01d6
Sean Anderson <sean@seananderson.ca>
2024-12-03 17:12:47 -0800

    Tweak all ones script

--- scratch/all-ones.R
+++ scratch/all-ones.R
@@ -64,7 +64,7 @@ fit <- sdmTMB(
 
 # compare to a different fixed value on n:
 .start2 <- rep(0, length(yrs))
-.start2[yrs == 2013] <- 30
+.start2[yrs == 2013] <- 10
 fit2 <- sdmTMB(
   density ~ 0 + factor(year),
   data = d,
@@ -80,32 +80,33 @@ fit2 <- sdmTMB(
   family = delta_gamma(type = "poisson-link")
 )
 
-.start3 <- rep(0, length(yrs))
-.start3[yrs == 2013] <- 5
-fit3 <- sdmTMB(
-  density ~ 0 + factor(year),
-  data = d,
-  mesh = mesh,
-  time = "year",
-  spatial = "on",
-  spatiotemporal = "off", # make the example fast
-  control =
-    sdmTMBcontrol(
-      map = list(b_j = .map),
-      start = list(b_j = .start3)
-    ),
-  family = delta_gamma(type = "poisson-link")
-)
-
 logLik(fit)
 logLik(fit2)
-logLik(fit3)
 
 fit$sd_report
 fit2$sd_report
-fit3$sd_report
+
+p1 <- predict(fit)
+p2 <- predict(fit2)
+
+plot(p1$est2, p1$est2);abline(0, 1)
+plot(p1$est1 + p1$est2, p2$est1 + p2$est2);abline(0, 1)
 
 # Conclusion: map the first linear predictor to any
-# value but note that it affects the scale of the
-# matching second linear predictor. In the end the
-# predictions and the model are the same.
+# large value but note that it affects the scale of the
+# matching second linear predictor
+
+pred1 <- predict(fit, newdata = nd, return_tmb_object = TRUE)
+pred2 <- predict(fit2, newdata = nd, return_tmb_object = TRUE)
+
+ind1 <- get_index(pred1, bias_correct = TRUE)
+ind2 <- get_index(pred2, bias_correct = TRUE)
+
+library(dplyr)
+ggplot(bind_rows(mutate(ind1, model = 1), mutate(ind2, model = 2)),
+  aes(year, est, ymin = lwr, ymax = upr)) +
+  geom_ribbon(fill = "grey50") +
+  geom_line(aes(colour = factor(model))) +
+  geom_vline(xintercept = 2013, lty = 2)
+
+ind1$est - ind2$est
