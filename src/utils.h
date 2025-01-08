770f1f3515e2e878698504d15c04ce5f4397bab2
jindivero <84645567+jindivero@users.noreply.github.com>
2025-01-08 15:45:46 -0800

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -313,9 +313,9 @@ Type linear_threshold(Type x, Type slope, Type cutpoint) {
   // as can cutpoint if covariate is scaled ~ N(0,1).
   Type pred;
   if (x < cutpoint) {
-    pred = x * slope;
+    pred = x * exp(slope);
   } else {
-    pred = cutpoint * slope;
+    pred = cutpoint * exp(slope);
   }
   return pred;
 }
