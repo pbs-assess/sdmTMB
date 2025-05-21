6608c4b3072bb2fd4d7c90b2f3a5020a180a6bac
Sean Anderson <sean@seananderson.ca>
2025-05-21 14:50:16 -0700

    Switch to citations README.md [skip ci]

--- scratch/citations/build.R
+++ scratch/citations/build.R
@@ -1,3 +1,15 @@
+# # Build sdmTMB citations list
+#
+# These are papers/theses/preprints/reports that use sdmTM
+# as a modelling platform.
+#
+# To build the list:
+#
+# 1. Add a reference to the Zotero collection.
+# 2. That collection will auto-export to the .bib files shown here.
+# 3. Source `build.R` in R.
+# 4. That will update the README.md file.
+
 setwd(here::here("scratch", "citations"))
 
 rmarkdown::render("journals.Rmd")
@@ -43,7 +55,9 @@ x <- gsub("\\}\\}", "", x)
 x <- gsub("\\{\\{", "", x)
 x <- gsub("\\}\\}", "", x)
 
-writeLines(x, "citations.md")
+x <- c(readLines("preamble.md"), "\n", x)
+
+writeLines(x, "README.md")
 rmarkdown::render("citations.md", output_file = "citations-preview.html")
 
 setwd(here::here("."))
